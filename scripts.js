2a4a4635ec4f249588a21e669376ed286636e262
Lisa Genner <116294867+LisaGenner@users.noreply.github.com>
2022-11-16 14:47:59 -0600

    Update scripts.js
    deleted previous code and added pokemon list

--- scripts.js
+++ scripts.js
@@ -1,8 +1,17 @@
-alert('Hello world');
-
-let favoriteFood = 'pizza';
-document.write(favoriteFood);
-alert(favoriteFood);
-
-
+let pokemonList = [
+    {
+      name: 'Butterfree',
+     height: 0.4,
+     type: ['bug']
+    },
+    {
+      name: 'Jigglyfuff',
+      height: 0.4,
+      type: ['fairy']
+    }
+    { name: 'Pikachu',
+      height: 0.4,
+      type: ['electric']
+  }
+     ];
 
