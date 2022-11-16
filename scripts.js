da5b2e526c09689cdc913d6d1bf1b23f03277264
Lisa Genner <116294867+LisaGenner@users.noreply.github.com>
2022-11-16 15:07:21 -0600

    Add files via upload
    added js script file

---
+++ scripts.js
@@ -0,0 +1,17 @@
+let pokemonList = [
+    {
+      name: 'Butterfree',
+     height: 0.4,
+     type: ['bug']
+    },
+    {
+      name: 'Jigglyfuff',
+      height: 0.4,
+      type: ['fairy']
+    }
+    { name: 'Pikachu',
+      height: 0.4,
+      type: ['electric']
+  }
+     ];
+
