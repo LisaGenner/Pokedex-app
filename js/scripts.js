216b56e302270ae9ebaec909432f3a9be719cda4
Lisa Genner <116294867+LisaGenner@users.noreply.github.com>
2022-11-23 10:02:34 -0600

    Update scripts.js
    updated js with revisions needed with Feedback.

--- js/scripts.js
+++ js/scripts.js
@@ -16,27 +16,6 @@ let pokemonListRepository =
      }
 ];
 
-//pokemon list
-let pokemonList = ['Bulbsaur', 'Charizard', 'Squirtle'];
-for (let i = 0; i < pokemonList.length; i++) {
-  document.write(pokemonList);
-}
-
-
-//document name + size
-let pokemonName = {name: 'Charizarde', size: 1.7};
-
-if  (pokemon.size > 1.5) {
-    document.write(pokemon.name  + 1.7);
-  }
-
-//wow! that is a large pokemon
-let pokemon = {name: 'Charizarde', size: 1.7};
-
-if  (pokemon.size > 1.5) {
-    document.write('this is a large pokemon');
-  }
-
   //wow! that's big
   let repository = [
     {name: 'Bulbsaur', height: 0.5},
@@ -47,7 +26,7 @@ if  (pokemon.size > 1.5) {
     for (let i=0; i < repository.length; i++) {
     if (repository[i].height <0.9) {
     document.write(repository[i].name + '<br>');
-    }else if (repository[i].height >1.6);{
+    }else if (repository[i].height >1.6) {
       document.write(repository[i].name +"1.6" +"that's a big pokemon" + '<br>')
     }
       }
