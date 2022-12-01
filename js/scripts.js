78055fef6c6ba4d840d52e9d21fc6deda37c4c69
Lisa Genner <116294867+LisaGenner@users.noreply.github.com>
2022-12-01 12:48:17 -0600

    Update scripts.js
    updated JS with feedback from 1.5

--- js/scripts.js
+++ js/scripts.js
@@ -1,77 +1,76 @@
-let pokemonListRepository =  
-[
+let pokemonListRepository =
+  [
     {
-    name: 'Bulbsaur',
-    height: 0.7,
-    types: ['grass', 'poison']
+      name: 'Bulbsaur',
+      height: 0.7,
+      types: ['grass', 'poison']
     },
     {
       name: 'Charizard',
       height: 1.7,
       types: ['fire', 'flying']
     },
-    { name: 'Squirtle',
+    {
+      name: 'Squirtle',
       height: 1,
       types: ['water']
-     }
-]; 
+    }
+  ];
+
+  let pokemonType =[
+    {name: "Butterfree", type: 'bug'},
+    {name: "Jigglyfuff", type: 'fairy'},
+    {name: "Pikachu", type: 'electric'}
+   ];
+
+for (let i=0; i < pokemonType.length; i++) {
+  if (pokemonType [i].type === 'electric') {
+    console.log(pokemonType[i].name + "is an electric pokemon");
+    } else if (pokemonType[i].type === 'bug')
+    {
+      console.log(pokemonType[i].name + "is an bug pokemon");
+   } else {
+      console.log(pokemonType[i].name + "is a fairy pokemon")
+   }};
+ 
 
 pokemonListRepository.forEach(function (user) {
   {
-    console.log(user.name + 'is' + user.height + ' tall');
+    document.write(user.name + ' is ' + user.height + ' tall');
   }
 });
 
 let pokemonRepository = (function () {
 
-  let pokemonList = [ 
-{
- name: 'Bulbsaur',
- height: 0.7,
- types: ['grass', 'poison']
- },
- {
-   name: 'Charizard',
-   height: 1.7,
-   types: ['fire', 'flying']
- }
-] 
+  let pokemonList = [
+    {
+      name: 'Bulbsaur',
+      height: 0.7,
+      types: ['grass', 'poison']
+    },
+    {
+      name: 'Charizard',
+      height: 1.7,
+      types: ['fire', 'flying']
+    },
+    {
+      name: 'Squirtle',
+      height: 1,
+      types: ['water']
+    }
+  ]
 
-function getAll () {
-return pokemonList;
-}
-function add (pokemon) {
-   pokemonList.push(pokemon);
-}
+  function getAll() {
+    return pokemonList;
+  }
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
 
-return {
-       getAll: getAll,
-add: add
-}
-}) ()
+  return {
+    getAll: getAll,
+    add: add
+  }
+}) ();
 
 console.log(pokemonRepository.getAll())
-
-
-
-
-
-
-
-
-
-     //wow! that's big
-  //let repository = [
-    //{name: 'Bulbsaur', height: 0.5},
-    //{name: 'Charizard', height: 1.7},
-   // {name: 'Squirtle', height: 0.7}
-    //];
-    
-    //for (let i=0; i < repository.length; i++) {
-    //if (repository[i].height <0.9) {
-    //document.write(repository[i].name + '<br>');
-    //}else if (repository[i].height >1.6) {
-     // document.write(repository[i].name +"1.6" +"that's a big pokemon" + '<br>')
-    //}
-     // }
-      
