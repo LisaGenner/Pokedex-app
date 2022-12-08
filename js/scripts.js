8e60a3bafc2e1824bd1ba69659b4d1edc0e379c6
Lisa Genner <116294867+LisaGenner@users.noreply.github.com>
2022-12-08 14:53:45 -0600

    Update scripts.js
    updated additional code for 1.6 exercise.

--- js/scripts.js
+++ js/scripts.js
@@ -17,21 +17,41 @@ let pokemonRepository = (function () {
     }
   ];
 
+
+  function add(pokemon) {
+    if (
+      typeof pokemon === "object" &&
+      "name" in pokemon &&
+      "height" in pokemon &&
+      "types" in pokemon
+    ) {
+      repository.push(pokemon);
+    } else {
+      console.log("pokemon is not correct");
+    }
+  }
   function getAll() {
     return repository;
-  }
+  };
+
+function showDetails(pokemon) {
+  console.log(pokemon)
+};
+
 function addListItem(pokemon) {
 //created new varible, defined in class HTML
 let pokemonList = document.querySelector (".pokemon-list"); 
-  
+}; 
 //created li element var
 let listpokemon = document.createElement("li"); 
 
 //created button tag
 let button = document.createElement("button");
+button.addEventListner('click'), function(event) {
+  showDetails(pokemon);
 
 //placeholder inside of button
-button.innerText = Pokemon.name; 
+button.innerText = pokemon.name; 
 //changed name in innertext to list actual name vs placeholder
 // button.innerText = pokemon.name;
 
@@ -44,11 +64,16 @@ pokemonList.appendChild(listpokemon);
 return {
     add: add,
     getAll: getAll,
-    addListItem : addListItem
+    addListItem : addListItem,
+    showDetails: showDetails,
   };
 
-})();
+
+pokemonRepository.add({ name: "Pikachu", height: 0.3, types: ["electric"] });
+
+console.log(pokemonRepository.getAll());
 
 pokemonRepository.getAll().forEach(function (pokemon) {
   pokemonRepository.addListItem(pokemon);
-});
+}
+})();
