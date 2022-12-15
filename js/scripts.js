12c756628d63d63f05a104e0f6bc6c73fa08bc00
Lisa Genner <116294867+LisaGenner@users.noreply.github.com>
2022-12-15 10:45:57 -0600

    Update scripts.js
    updated js with mentor to make code work.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 let pokemonRepository = (function () {
   let pokemonList = [];
-  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=3';
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=20';
+  let pokemonListElement = document.querySelector('.pokemon-list');
 
   function add(pokemon) {
     if (
@@ -30,7 +31,7 @@ let pokemonRepository = (function () {
     button.addEventListener("click", function (event) { //go to the button and i want to add the event.  user can click on the button, i want to do something i want to run this fuction that is inside the function line 30-32
       showDetails(pokemon);
     });
-  }
+  };
 
   //other functions remain here
 
@@ -59,19 +60,64 @@ let pokemonRepository = (function () {
       return response.json();
     }).then(function (details) {
       // Now we add the details to the item
+      // console.log(details);
       item.imageUrl = details.sprites.front_default;
       item.height = details.height;
       item.types = details.types;
+      return item; //need to return the item
     }).catch(function (e) {
       console.error(e);
     });
   }
 
-  function showDetails(pokemon) {
-    loadDetails(pokemon).then(function () {
-      console.log(pokemon);
-    });
+
+  //console.log(pokemon);
+
+  //Modal code/////
+
+  function showModal(pokemon) {
+    let modalContainer = document.querySelector('.modal-container'); //creating a reference var called modal container and grabing modal-cont html element assigning/saving to the var
+    modalContainer.innerText = '';
+
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = pokemon.name;
+
+    let pokemonHeight = document.createElement('p');
+    pokemonHeight.innerText = "Height: " + pokemon.height;
+
+    let pokemonImage = document.createElement('img');
+    pokemonImage.src = pokemon.imageUrl;
+
+    modal.appendChild(titleElement);
+    modal.appendChild(pokemonHeight);
+    modal.appendChild(pokemonImage);
+    modalContainer.appendChild(modal);
+
+    modalContainer.addEventListener('click', (e) => {
+      let target = e.target;
+      if (target === modalContainer) {
+        hideModal();
+      }
+    })
+
+    modalContainer.classList.add('is-visible');
   }
+
+  function hideModal() {
+    let modalContainer = document.querySelector('.modal-container'); 
+    modalContainer.classList.remove('is-visible');
+  }
+
+  window.addEventListener('keydown', (e) => {
+    let modalContainer = document.querySelector('.modal-container');
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();
+    }
+  });
+
 
   return {
     add: add,
@@ -79,16 +125,18 @@ let pokemonRepository = (function () {
     addListItem: addListItem,
     loadList: loadList,
     loadDetails: loadDetails,
-    showDetails: showDetails
+    showDetails: showDetails,
+    showModal: showModal
   };
 })();
 
-
-// pokemonRepository.add({
-// name: "Pikachu", height: 0.3, types: ["electric"]
-// });
-
-// console.log(pokemonRepository.getAll());
+function showDetails(item) {
+  console.log(item);
+  pokemonRepository.loadDetails(item).then(function (pokemon) {
+    console.log(pokemon); //need to return what we want from the function .url
+    pokemonRepository.showModal(pokemon); //since i have showModal inside the pokemonRespository function i need to return it from the function
+  });
+}
 
 pokemonRepository.loadList().then(function () {
   pokemonRepository.getAll().forEach(function (pokemon) {
